634aa68858f9abbf5d53b1a2bfee58571925695c
LENOVO <LENOVO@krishna>
2022-09-04 15:08:27 -0700

    second

--- src/main/java/pack/Firstclass.java
+++ src/main/java/pack/Firstclass.java
@@ -5,7 +5,7 @@ public class Firstclass {
 	public static void main(String[] args) {
 	
 		System.out.println("swapnil first git hub code");
-		
+		System.out.println("swapnil second git hub code");
 		
 	}
 
